69308c3bfac7245445c8566b02a2334c4a92c427
Samidha09 <samidha.verma1996@gmail.com>
2019-04-30 11:09:19 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,127 @@
-require('./config/config');
-require('./models/db');
-require('./config/passportConfig');
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const passport = require('passport');
-
-const rtsIndex = require('./routes/index.router');
-
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+var express = require("express");
+var path = require('path');
 var app = express();
+var port = 3000;
 
-// middleware
+//APP CONFIG
+app.use(express.static('./'));
 app.use(bodyParser.json());
-app.use(cors());
-app.use(passport.initialize());
-app.use('/api', rtsIndex);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'html');
+app.set('views', __dirname + '/');
+// app.engine('html', require('ejs').renderFile);
+var MongoClient= mongodb.MongoClient;
+//MONGOOSE/MODEL CONFIG
+// var conn = mongoose.connect("mongodb://localhost:27017/seller-shipment", { useNewUrlParser: true});
+// var shipmentSchema = new mongoose.Schema({
+//   itemName: String,
+//   quantity:String,
+//   sourceFloor: String,
+//   sourceStreet: String,
+//   sourceCity: String,
+//   sourceState: String,
+//   sourcePinCode: String,
+//   destFloor: String,
+//   destStreet: String,
+//   destCity: String,
+//   destState: String,
+//   destPinCode: String,
+//   shippingDate: Date
+// });
 
-// error handler
-app.use((err, req, res, next) => {
-    if (err.name === 'ValidationError') {
-        var valErrors = [];
-        Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
-        res.status(422).send(valErrors)
-    }
+// mongoose.model("seller-shipment", shipmentSchema);
+
+//ROUTES
+//Home 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// start server
-app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
+//Shipment Form
+app.get('/ship', function(req, res){
+ res.sendFile(path.join(__dirname, 'ship.html'));
+});
+
+app.post('/ship', function(req, res, next){
+    var receivedObject = req.query;
+    console.log(receivedObject);
+
+    var url = 'mongodb://localhost:27017/seller-shipment';
+    MongoClient.connect(url, function(err, db){
+      if(err)
+      {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+      } 
+      else 
+      {
+         console.log('Connection established to', url);
+         //get the documents collection
+         var collection = db.collection('seller-shipment');
+         //get the orders
+         collection.find().toArray(function(err, result){
+           if(err){
+             console.log(err);
+           } else{
+             console.log('Got Information from DB!');
+           }
+  
+           var newOrder = receivedObject;
+           var collection = db.collection('seller-shipment');
+           collection.insert([newOrder], function(err, result){
+             if(err){
+               console.log(err);
+             } else{
+               console.log('INSERTED ORDER INTO COLLECTION!!', result);
+             }
+           });
+           //close connection
+           db.close();
+         });
+      }   
+    });
+
+    res.redirect("/shipmentMade");
+ });
+
+//Shipment redirect
+app.get('/shipmentMade', function(req, res){
+  res.sendFile(path.join(__dirname, 'shipment_made.html'));
+});
+
+app.get('/retreiveAll', function(req, res){
+  var url = 'mongodb://localhost:27017/seller-shipment';
+  MongoClient.connect(url, function(err, db){
+    if(err)
+    {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } 
+    else 
+    {
+       console.log('Connection established to', url);
+       //get the documents collection
+       var collection = db.collection('seller-shipment');
+       //get the orders
+       collection.find().toArray(function(err, result){
+         if(err){
+           console.log(err);
+         } else{
+           console.log('Got Information from DB!');
+         }
+
+         //close connection
+         db.close();
+         console.log('db closed');
+         var orders = result;
+         res.send({'orders': orders});
+         console.log(orders);
+       });
+    }   
+  });
+});
+
+
+app.listen(port, function(res, req) {
+  console.log("Server listening on port " + port);
+});
